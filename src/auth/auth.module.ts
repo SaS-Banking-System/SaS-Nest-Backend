42127627d6884c55d53b5548a5318d5649b2e80c
johannes-baehr <johannes.baehr@gmx.net>
2023-11-30 19:37:08 +0100

    implemented auth guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,7 +9,7 @@ import 'dotenv/config';
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '5m' },
     }),
   ],
   controllers: [AuthController],
