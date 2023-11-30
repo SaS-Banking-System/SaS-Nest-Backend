6cdd9efa18c4db6be9ec8a9f334632d24550a46c
johannes-baehr <johannes.baehr@gmx.net>
2023-11-30 14:30:38 +0100

    organized app with modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
-import { PrismaService } from './prisma/prisma.service';
-import { TransactionController } from './transaction/transaction.controller';
-import { TransactionService } from './transaction/transaction.service';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
+import { UserModule } from './user/user.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [
-    AppController,
-    UserController,
-    TransactionController,
-    AuthController,
-  ],
-  providers: [
-    AppService,
-    UserService,
-    PrismaService,
-    TransactionService,
-    AuthService,
-  ],
+  imports: [UserModule, TransactionModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,7 +4,7 @@ import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) { }
+  constructor(private readonly authService: AuthService) {}
 
   @Post()
   @HttpCode(HttpStatus.OK)
