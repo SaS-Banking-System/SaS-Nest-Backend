3848f479b8d55353651cec501c450865c7833dee
best2327 <bkaya1007@gmail.com>
2024-03-16 19:42:49 +0100

    - added private transaction boolean field

---
+++ prisma/migrations/20240316183701_added_private_transaction_boolean_but_this_time_fo_shure/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - Added the required column `private` to the `Transaction` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Transaction" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "sender" TEXT NOT NULL,
+    "receiver" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "tax" INTEGER DEFAULT 0,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "private" BOOLEAN NOT NULL
+);
+INSERT INTO "new_Transaction" ("amount", "createdAt", "id", "receiver", "sender", "tax") SELECT "amount", "createdAt", "id", "receiver", "sender", "tax" FROM "Transaction";
+DROP TABLE "Transaction";
+ALTER TABLE "new_Transaction" RENAME TO "Transaction";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
